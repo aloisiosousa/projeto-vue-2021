04a38869cf391045504167ee543729bba760d0c7
aloisiosousa <32015500+aloisiosousa@users.noreply.github.com>
2021-05-17 18:28:05 -0300

    Testando Ação do Hello do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Iniciando o Projeto de Vue.js - 2021 App"/>
   </div>
 </template>
 
